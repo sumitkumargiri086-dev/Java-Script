1705bd97fb88da139a08a4513a516f09d909041c
sumit kumar giri <sumitkumargiri086@gmail.com>
2026-08-16 23:58:24 +0530

    variable and data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+console.log("Sumit")
+const Id=12345;
+let name="Sumit Kumar Giri";
+var city="Gopalganj";
+state="Bihar";
+
+console.log(Id);
+
+/* we alsway use const  or let not use
+var more time*/
+
+console.table([Id,name,city,state]);
